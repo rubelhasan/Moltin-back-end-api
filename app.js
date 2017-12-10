ed7922ff528285b05a1f3000f48f11f08907de6f
Rubel <rubel@statup.com>
2017-12-10 14:13:51 +0600

    add eslint for maintain code quality

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 
-const api_endpoint=process.env.API_ENDPOINT || '3000';
-const db_endpoint=process.env.DB || '8081';
-const server=require('./server').server;
-const Route=require('router');
-const router=Route();
+const apiEndpoint = process.env.API_ENDPOINT || '3000'
+const dbEndpoint = process.env.DB || '8081'
+const Server = require('./server').server
+const Route = require('router')
+const router = Route()
 
-const product=require('./api/product').product;
-const item=new product(router);
-const project=new server(router);
-item.getProductList();
-item.getProduct();
-project.run().listen(api_endpoint);
-
-
-
-
-console.log(`server running: ${api_endpoint}`);	
+const Product = require('./api/product').product
+const item = new Product(router)
+const project = new Server(router)
+item.getProductList()
+item.getProduct()
+project.run().listen(apiEndpoint)
+console.log(`server running: ${apiEndpoint}`)
